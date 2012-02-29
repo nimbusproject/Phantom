784d6e1ff4a30b310a1b458e19b5aed5394a5618
BuzzTroll <buzztroll@gmail.com>
2012-02-29 09:48:04 -1000

    erant key stroke

--- pyhantom/system/epu_localdb/epu_system.py
+++ pyhantom/system/epu_localdb/epu_system.py
@@ -91,7 +91,7 @@ class EPUSystemWithLocalDB(SystemLocalDB):
         conf['engine_conf']['preserve_n'] = asg.DesiredCapacity
         conf['engine_conf']['epuworker_image_id'] = db_lc.ImageId
         conf['engine_conf']['iaas_key'] = user_obj.username
-        conf['engine_conf']['iaas_secret'] = user_obj.passwor
+        conf['engine_conf']['iaas_secret'] = user_obj.password
         conf['engine_conf']['iaas_site'] = db_asg.AvailabilityZones + " " + user_obj.username
         conf['engine_conf']['iaas_allocation'] = db_lc.InstanceType
 
