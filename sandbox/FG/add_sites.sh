05850f7df570359285fced89f2b99f5d46cf999e
BuzzTroll <buzztroll@gmail.com>
2012-05-15 08:43:28 -1000

    removing hardcoded paths

--- sandbox/FG/add_sites.sh
+++ sandbox/FG/add_sites.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+thisdir=`dirname $0`
 sites="hotel alamo sierra foxtrot"
 
 if [ -z $PHANTOM_EPU_RABBIT_HOST ]; then
@@ -9,7 +10,7 @@ fi
 
 for site in $sites
 do
-    $PHANTOM_CEICTL_DIR/bin/ceictl -b $PHANTOM_EPU_RABBIT_HOST -x $EXCHANGE_SCOPE -u $RABBITMQ_USERNAME -p $RABBITMQ_PASSWORD site add $site --definition /home/bresnaha/Dev/Nimbus/Phantom/phantom/sandbox/FG/$site.yml
+    $PHANTOM_CEICTL_DIR/bin/ceictl -b $PHANTOM_EPU_RABBIT_HOST -x $EXCHANGE_SCOPE -u $RABBITMQ_USERNAME -p $RABBITMQ_PASSWORD site add $site --definition $thisdir/$site.yml
 
 done
 
