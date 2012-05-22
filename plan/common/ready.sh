5ff018482317f52a5b3584c3c300b773eaaa34a6
Pierre Riteau <priteau@ci.uchicago.edu>
2012-05-22 11:56:05 +0200

    Add readypgm programs for other levels

--- plan/common/ready.sh
+++ plan/common/ready.sh
@@ -3,11 +3,11 @@
 set -e
 
 APP_DIR=`cat bootconf.json | python -c "import sys; import json; j = json.loads(sys.stdin.read()); print j['appdir']"`
-APP_NAME=`cat bootconf.json | python -c "import sys; import json; j = json.loads(sys.stdin.read()); print j['appname']"`
+PROCESS_NAME=`cat bootconf.json | python -c "import sys; import json; j = json.loads(sys.stdin.read()); print j['process_name']"`
 VENV_PATH=`cat bootconf.json | python -c "import sys; import json; j = json.loads(sys.stdin.read()); print j['virtualenv']['path']"`
 
 set +e
-STATUS=`sudo -u epu sh -c ". $VENV_PATH/bin/activate; supervisorctl -c $APP_DIR/supervisor.conf status $APP_NAME"`
+STATUS=`sudo -u epu sh -c ". $VENV_PATH/bin/activate; supervisorctl -c $APP_DIR/supervisor.conf status $PROCESS_NAME"`
 echo $STATUS | grep RUNNING > /dev/null
 if [ $? != 0 ]; then
   echo $STATUS
