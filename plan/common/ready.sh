55320d1243d921a4602f500205f4c03f0c3e60f0
Pierre Riteau <priteau@ci.uchicago.edu>
2012-06-12 00:08:00 +0200

    Missing quote

--- plan/common/ready.sh
+++ plan/common/ready.sh
@@ -3,7 +3,7 @@
 set -e
 
 APP_DIR=`cat bootconf.json | python -c "import sys; import json; j = json.loads(sys.stdin.read()); print j['epu']['run_config']['run_directory']"`
-PROCESS_NAME=`cat bootconf.json | python -c "import sys; import json; j = json.loads(sys.stdin.read()); print j['epu]['run_config']['program']"`
+PROCESS_NAME=`cat bootconf.json | python -c "import sys; import json; j = json.loads(sys.stdin.read()); print j['epu']['run_config']['program']"`
 VENV_PATH=`cat bootconf.json | python -c "import sys; import json; j = json.loads(sys.stdin.read()); print j['epu']['virtualenv']['path']"`
 
 set +e
