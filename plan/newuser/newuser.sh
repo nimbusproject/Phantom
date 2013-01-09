68739d388922eec487c2eaa8364278a4e95d9bfc
Pierre Riteau <priteau@ci.uchicago.edu>
2013-01-09 18:17:11 +0000

    Execute the new user command as www-data
    This allows log log permissions to stay as www-data instead of changing
    to root, which makes the web app fail.

--- plan/newuser/newuser.sh
+++ plan/newuser/newuser.sh
@@ -14,4 +14,4 @@ su epu sh -c "source /home/epu/app-venv/bin/activate && ./test_add_user.py $PHAN
 su epu sh -c "source /home/epu/app-venv/bin/activate && ./add_mysql_user.py $PHANTOM_USERNAME $PHANTOM_IAAS_ACCESS_KEY $PHANTOM_IAAS_SECRET_KEY /home/epu/phantom/phantomautoscale.yml"
 
 cd /home/epu/phantomweb
-./bin/phantomweb-new-user.py $PHANTOM_USERNAME $PHANTOM_USER_EMAIL $PHANTOM_IAAS_SECRET_KEY
+su www-data sh -c "./bin/phantomweb-new-user.py $PHANTOM_USERNAME $PHANTOM_USER_EMAIL $PHANTOM_IAAS_SECRET_KEY"
