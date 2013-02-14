9861876fb568f93b9a8d3c04ca33375cf3a6af71
Pierre Riteau <priteau@ci.uchicago.edu>
2013-02-14 16:59:30 +0000

    Switch from boto fork to upstream boto version 2.8.0

--- setup.py
+++ setup.py
@@ -25,9 +25,8 @@ setup(name='pyhantom',
       license="Apache2",
       dependency_links=[
           'https://github.com/nimbusproject/ceiclient/archive/master.tar.gz#egg=ceiclient-0.1',
-          'https://github.com/nimbusproject/PhantomSQL/archive/master.tar.gz#egg=phantomsql-0.2.1',
-          'https://github.com/buzztroll/boto/archive/non_aws_asg_connection.tar.gz#egg=boto-2.6'],
-      install_requires = ["simplejson == 2.3.2", "boto == 2.6", "dashi", "ceiclient == 0.1", "sqlalchemy == 0.7.6", "wsgiref", "webob", "cherrypy >= 3.2", "mysql-python == 1.2.3", "phantomsql == 0.2.1"],
+          'https://github.com/nimbusproject/PhantomSQL/archive/master.tar.gz#egg=phantomsql-0.2.1'],
+      install_requires = ["simplejson == 2.3.2", "boto == 2.8.0", "dashi", "ceiclient == 0.1", "sqlalchemy == 0.7.6", "wsgiref", "webob", "cherrypy >= 3.2", "mysql-python == 1.2.3", "phantomsql == 0.2.1"],
       entry_points = {
         'console_scripts': [
             'phantomwsgiref = pyhantom.execs.simpleref:main',
