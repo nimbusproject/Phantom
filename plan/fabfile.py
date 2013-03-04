a8df0c598619605de0efb0ccb33c9860f2d89510
Pierre Riteau <priteau@ci.uchicago.edu>
2013-03-04 17:04:35 +0000

    Add a fabfile with a task to rerun Chef cookbooks

---
+++ plan/fabfile.py
@@ -0,0 +1,12 @@
+from fabric.api import env, run
+
+env.user = 'root'
+
+def update():
+    print("Executing on %(host)s as %(user)s" % env)
+    run('/opt/rerun-chef-basenode.sh')
+    run('/opt/rerun-chef-dtrs.sh')
+    run('/opt/rerun-chef-provisioner-start.sh')
+    run('/opt/rerun-chef-epum.sh')
+    run('/opt/rerun-chef-phantom.sh')
+    run('/opt/rerun-chef-phantomweb.sh')
