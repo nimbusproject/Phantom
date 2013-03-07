5ab5a1720cdcac4808976f7833312ab4dfc75774
Pierre Riteau <priteau@ci.uchicago.edu>
2013-03-07 17:12:15 +0000

    Alamo has changed hostname

--- sandbox/FG/test_add_user.py
+++ sandbox/FG/test_add_user.py
@@ -55,7 +55,7 @@ def add_one_user(dtrs_client, access_key, access_secret, pub_key, username):
             'secret_key': access_secret,
             'key_name': phantomkey_name}
 
-    hosts = {"hotel": "https://svc.uc.futuregrid.org:8444", "sierra" : "https://s83r.idp.sdsc.futuregrid.org:8444", "alamo": "https://master1.futuregrid.tacc.utexas.edu:8444", "foxtrot": "https://f1r.idp.ufl.futuregrid.org:9444"}
+    hosts = {"hotel": "https://svc.uc.futuregrid.org:8444", "sierra" : "https://s83r.idp.sdsc.futuregrid.org:8444", "alamo": "https://nimbus.futuregrid.tacc.utexas.edu:8444", "foxtrot": "https://f1r.idp.ufl.futuregrid.org:9444"}
     print "public key is %s" % (pub_key)
     for host in hosts:
         try:
