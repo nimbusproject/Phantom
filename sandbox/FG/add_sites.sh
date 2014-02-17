5693b7585b19a15a75761cd02cf5ac747ffc123c
Pierre Riteau <priteau@ci.uchicago.edu>
2014-02-17 16:30:15 +0000

    Add OpenStack Hotel cloud to the list

--- sandbox/FG/add_sites.sh
+++ sandbox/FG/add_sites.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 thisdir=`dirname $0`
-sites="alamo-openstack alamo ec2-eu ec2 foxtrot hotel india-openstack sierra-openstack sierra"
-#sites="ec2"
+sites="alamo-openstack alamo ec2-eu ec2 foxtrot hotel hotel-openstack india-openstack sierra-openstack sierra"
 
 if [ -z $PHANTOM_EPU_RABBIT_HOST ]; then
     echo "Please setup the environment"
@@ -12,7 +11,7 @@ fi
 for site in $sites
 do
     cmd="ceictl -b $PHANTOM_EPU_RABBIT_HOST -x $EXCHANGE_SCOPE -u $RABBITMQ_USERNAME -p $RABBITMQ_PASSWORD site add $site --definition $thisdir/$site.yml"
-    echo $cmd   
+    echo $cmd
     $cmd
 done
 
